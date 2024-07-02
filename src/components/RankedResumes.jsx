620cd6d0fa35556fdd6b04688fdce39da704bf24
Muhammad Abdullah <abdullahfalak007@gmail.com>
2024-07-02 12:24:26 +0500

    Ranked Resume Box Finalized

--- src/components/RankedResumes.jsx
+++ src/components/RankedResumes.jsx
@@ -1,106 +1,3 @@
-import React from "react";
-import PropTypes from "prop-types";
-import imagePaths from "../data/imagePaths.json"; // Import the image paths
-
-const RankedResumes = ({ resumeData }) => {
-  return (
-    <div className="flex-grow p-8">
-      <h2 className="text-3xl font-bold mb-6 text-center">Ranked Resumes</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {resumeData.map((resume, index) => (
-          <div
-            key={index}
-            className="p-6 border rounded-lg shadow-lg bg-[#E4F0FF] relative"
-            style={{
-              borderRadius: "17.84px",
-              width: "368.22px",
-              height: "370.13px",
-            }}
-          >
-            <div className="absolute top-4 right-0 mr-[-1rem] w-[81.52px] h-[28.82px] rounded-tl-[8.23px] rounded-bl-[8.23px] bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] flex items-center justify-center">
-              <span className="text-white font-airbnbCereal_W_Bd">
-                {resume.rankingIndex}% Rank
-              </span>
-            </div>
-            <div className="flex items-center mb-4">
-              <img
-                src={resume.image}
-                alt={`${resume.name}'s profile`}
-                className="object-cover rounded-full mr-4 border-4 border-[#0362B3]"
-                style={{
-                  width: "45.4px",
-                  height: "45.4px",
-                }}
-              />
-              <div>
-                <h3
-                  className="font-comfortaaBold"
-                  style={{
-                    fontWeight: 700,
-                    fontSize: "19.46px",
-                  }}
-                >
-                  {resume.name}
-                </h3>
-                <p
-                  className="text-gray-600 flex items-center font-comfortaaMedium"
-                  style={{
-                    fontWeight: 500,
-                    fontSize: "12.97px",
-                  }}
-                >
-                  <img
-                    src={imagePaths.location}
-                    alt="Location icon"
-                    className="h-5 w-5 mr-1"
-                  />
-                  {resume.location}
-                </p>
-              </div>
-            </div>
-            <div className="mb-4">
-              <h4 className="font-semibold">Skills</h4>
-              <p className="text-customBlue">{resume.skills}</p>
-            </div>
-            <div className="mb-4">
-              <h4 className="font-semibold">Total Experience</h4>
-              <p className="text-customBlue">{resume.totalExperience}</p>
-            </div>
-            <div className="mb-4">
-              <h4 className="font-semibold">Education</h4>
-              <p className="text-customBlue">{resume.education}</p>
-            </div>
-            <div className="flex justify-between">
-              <button className="bg-[#0362B3] text-white py-2 px-4 rounded-md hover:bg-[#024b89] transition">
-                View
-              </button>
-              <button className="bg-[#0362B3] text-white py-2 px-4 rounded-md hover:bg-[#024b89] transition">
-                Download
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-RankedResumes.propTypes = {
-  resumeData: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      location: PropTypes.string.isRequired,
-      rankingIndex: PropTypes.number.isRequired,
-      skills: PropTypes.string.isRequired,
-      totalExperience: PropTypes.string.isRequired,
-      education: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired, // Added image prop type
-    })
-  ).isRequired,
-};
-
-export default RankedResumes;
-
 // import React from "react";
 // import PropTypes from "prop-types";
 // import imagePaths from "../data/imagePaths.json"; // Import the image paths
@@ -120,31 +17,31 @@ export default RankedResumes;
 //               height: "370.13px",
 //             }}
 //           >
-//             <div className="absolute top-4 right-4 flex space-x-2">
+//             <div className="absolute top-4 right-0 flex flex-col space-y-2">
 //               <div
 //                 className="flex items-center justify-center"
 //                 style={{
 //                   width: "81.52px",
 //                   height: "28.82px",
-//                   borderRadius: "8.23px 0 0 0",
+//                   borderRadius: "8.23px 0 0 8.23px",
 //                   background: "linear-gradient(to right, #0362B3, #4E9BCF)",
 //                 }}
 //               >
 //                 <img
-//                   src={imagePaths.aiIcon} // Ensure you have the correct path for the AI icon
+//                   src={imagePaths.askAI} // Ensure you have the correct path for the AI icon
 //                   alt="AI Icon"
 //                   className="w-4 h-4 mr-1"
 //                 />
-//                 <span className="text-white font-airbnbCereal_W_Bd">
+//                 <span className="text-white font-airbnbCereal_W_Bd text-[12.35px]">
 //                   Ask AI
 //                 </span>
 //               </div>
 //               <button
-//                 className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-3 rounded-md font-airbnbCereal_W_Bd"
+//                 className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-3 rounded-md font-airbnbCereal_W_Bd text-[12.35px]"
 //                 style={{
 //                   width: "81.52px",
 //                   height: "28.82px",
-//                   borderRadius: "0 8.23px 0 0",
+//                   borderRadius: "8.23px 0 0 8.23px",
 //                 }}
 //               >
 //                 {resume.rankingIndex}% Rank
@@ -154,19 +51,17 @@ export default RankedResumes;
 //               <img
 //                 src={resume.image}
 //                 alt={`${resume.name}'s profile`}
-//                 className="object-cover rounded-full mr-4"
+//                 className="object-cover rounded-full mr-4 mt-2 border-4 border-[#0362B3]"
 //                 style={{
 //                   width: "45.4px",
 //                   height: "45.4px",
-//                   border: "3.24px solid #0362B3",
-//                   borderRadius: "50%",
 //                 }}
 //               />
 //               <div>
 //                 <h3
 //                   className="font-comfortaaBold"
 //                   style={{
-//                     fontWeight: 700,
+//                     fontWeight: 400,
 //                     fontSize: "19.46px",
 //                   }}
 //                 >
@@ -175,7 +70,7 @@ export default RankedResumes;
 //                 <p
 //                   className="text-gray-600 flex items-center font-comfortaaMedium"
 //                   style={{
-//                     fontWeight: 500,
+//                     fontWeight: 200,
 //                     fontSize: "12.97px",
 //                   }}
 //                 >
@@ -189,16 +84,65 @@ export default RankedResumes;
 //               </div>
 //             </div>
 //             <div className="mb-4">
-//               <h4 className="font-semibold">Skills</h4>
-//               <p className="text-customBlue">{resume.skills}</p>
+//               <h4
+//                 className="font-comfortaaLight"
+//                 style={{
+//                   fontWeight: 200,
+//                   fontSize: "12.97px",
+//                 }}
+//               >
+//                 Skills
+//               </h4>
+//               <div
+//                 className="text-customBlue font-comfortaaLight"
+//                 style={{ fontWeight: 400, fontSize: "19.46px" }}
+//               >
+//                 {resume.skills.split(",").map((skill, i) => (
+//                   <span key={i} className="mr-3">
+//                     {skill.trim()}
+//                   </span>
+//                 ))}
+//               </div>
 //             </div>
 //             <div className="mb-4">
-//               <h4 className="font-semibold">Total Experience</h4>
-//               <p className="text-customBlue">{resume.totalExperience}</p>
+//               <h4
+//                 className="font-comfortaaLight"
+//                 style={{
+//                   fontWeight: 200,
+//                   fontSize: "12.97px",
+//                 }}
+//               >
+//                 Total Experience
+//               </h4>
+//               <p
+//                 className="text-customBlue font-comfortaaLight"
+//                 style={{
+//                   fontWeight: 400,
+//                   fontSize: "19.46px",
+//                 }}
+//               >
+//                 {resume.totalExperience}
+//               </p>
 //             </div>
 //             <div className="mb-4">
-//               <h4 className="font-semibold">Education</h4>
-//               <p className="text-customBlue">{resume.education}</p>
+//               <h4
+//                 className="font-comfortaaLight"
+//                 style={{
+//                   fontWeight: 500,
+//                   fontSize: "12.97px",
+//                 }}
+//               >
+//                 Education
+//               </h4>
+//               <p
+//                 className="text-customBlue font-comfortaaLight"
+//                 style={{
+//                   fontWeight: 500,
+//                   fontSize: "19.46px",
+//                 }}
+//               >
+//                 {resume.education}
+//               </p>
 //             </div>
 //             <div className="flex justify-between">
 //               <button className="bg-[#0362B3] text-white py-2 px-4 rounded-md hover:bg-[#024b89] transition">
@@ -230,3 +174,196 @@ export default RankedResumes;
 // };
 
 // export default RankedResumes;
+
+import React from "react";
+import PropTypes from "prop-types";
+import imagePaths from "../data/imagePaths.json"; // Import the image paths
+
+const RankedResumes = ({ resumeData }) => {
+  return (
+    <div className="flex-grow p-8">
+      <h2 className="text-3xl font-bold mb-6 text-center">Ranked Resumes</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        {resumeData.map((resume, index) => (
+          <div
+            key={index}
+            className="p-6 border rounded-lg shadow-lg bg-[#E4F0FF] relative"
+            style={{
+              borderRadius: "17.84px",
+              width: "368.22px",
+              height: "370.13px",
+            }}
+          >
+            <div className="absolute top-4 right-0 flex flex-col space-y-2">
+              <div
+                className="flex items-center justify-center"
+                style={{
+                  width: "81.52px",
+                  height: "28.82px",
+                  borderRadius: "8.23px 0 0 8.23px",
+                  background: "linear-gradient(to right, #0362B3, #4E9BCF)",
+                }}
+              >
+                <img
+                  src={imagePaths.askAI} // Ensure you have the correct path for the AI icon
+                  alt="AI Icon"
+                  className="w-4 h-4 mr-1"
+                />
+                <span className="text-white font-airbnbCereal_W_Bd text-[12.35px]">
+                  Ask AI
+                </span>
+              </div>
+              <button
+                className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-3 rounded-md font-airbnbCereal_W_Bd text-[12.35px]"
+                style={{
+                  width: "81.52px",
+                  height: "28.82px",
+                  borderRadius: "8.23px 0 0 8.23px",
+                }}
+              >
+                {resume.rankingIndex}% Rank
+              </button>
+            </div>
+            <div className="flex items-center mb-4">
+              <img
+                src={resume.image}
+                alt={`${resume.name}'s profile`}
+                className="object-cover rounded-full mr-4 mt-2 border-4 border-[#0362B3]"
+                style={{
+                  width: "45.4px",
+                  height: "45.4px",
+                }}
+              />
+              <div>
+                <h3
+                  className="font-comfortaaBold"
+                  style={{
+                    fontWeight: 400,
+                    fontSize: "19.46px",
+                  }}
+                >
+                  {resume.name}
+                </h3>
+                <p
+                  className="text-gray-600 flex items-center font-comfortaaMedium"
+                  style={{
+                    fontWeight: 200,
+                    fontSize: "12.97px",
+                  }}
+                >
+                  <img
+                    src={imagePaths.location}
+                    alt="Location icon"
+                    className="h-5 w-5 mr-1"
+                  />
+                  {resume.location}
+                </p>
+              </div>
+            </div>
+            <div className="mb-4">
+              <h4
+                className="font-comfortaaLight"
+                style={{
+                  fontWeight: 200,
+                  fontSize: "12.97px",
+                }}
+              >
+                Skills
+              </h4>
+              <div
+                className="text-customBlue font-comfortaaLight"
+                style={{ fontWeight: 400, fontSize: "19.46px" }}
+              >
+                {resume.skills.split(",").map((skill, i) => (
+                  <span key={i} className="mr-3">
+                    {skill.trim()}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className="mb-4">
+              <h4
+                className="font-comfortaaLight"
+                style={{
+                  fontWeight: 200,
+                  fontSize: "12.97px",
+                }}
+              >
+                Total Experience
+              </h4>
+              <p
+                className="text-customBlue font-comfortaaLight"
+                style={{
+                  fontWeight: 400,
+                  fontSize: "19.46px",
+                }}
+              >
+                {resume.totalExperience}
+              </p>
+            </div>
+            <div className="mb-4">
+              <h4
+                className="font-comfortaaLight"
+                style={{
+                  fontWeight: 500,
+                  fontSize: "12.97px",
+                }}
+              >
+                Education
+              </h4>
+              <p
+                className="text-customBlue font-comfortaaLight"
+                style={{
+                  fontWeight: 500,
+                  fontSize: "19.46px",
+                }}
+              >
+                {resume.education}
+              </p>
+            </div>
+            <div className="flex justify-center space-x-4 mt-4">
+              <button
+                className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-4 rounded-md font-airbnbCereal_W_Bd hover:bg-[#024b89] transition flex items-center justify-center"
+                style={{
+                  width: "91.82px",
+                  height: "28.82px",
+                  borderRadius: "8.23px",
+                  border: "0.82px solid #0362B3",
+                }}
+              >
+                View
+              </button>
+              <button
+                className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-4 rounded-md font-airbnbCereal_W_Bd hover:bg-[#024b89] transition flex items-center justify-center"
+                style={{
+                  width: "91.82px",
+                  height: "28.82px",
+                  borderRadius: "8.23px",
+                  border: "0.82px solid #0362B3",
+                }}
+              >
+                Download
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+RankedResumes.propTypes = {
+  resumeData: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      rankingIndex: PropTypes.number.isRequired,
+      skills: PropTypes.string.isRequired,
+      totalExperience: PropTypes.string.isRequired,
+      education: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired, // Added image prop type
+    })
+  ).isRequired,
+};
+
+export default RankedResumes;
