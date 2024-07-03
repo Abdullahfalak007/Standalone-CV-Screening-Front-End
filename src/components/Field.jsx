3ad9f5802d4f3ead82a66a4092a106157e0da3df
Muhammad Abdullah <abdullahfalak007@gmail.com>
2024-07-03 16:16:39 +0500

    Dropdown arrow on the Upload resume Page, inside the Field.jsx, has bee fixed

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,5 +1,49 @@
+// import React from "react";
+// import dropdownsData from "../data/dropdownsData.json";
+
+// const Field = () => {
+//   return (
+//     <div className="py-8">
+//       <div className="container mx-auto p-4">
+//         <div className="flex flex-col md:flex-row md:space-x-4">
+//           <div className="flex flex-col space-y-4 md:w-[358px]">
+//             <div className="form-control">
+//               <label className="label text-customBlue">Job Title*</label>
+//               <input
+//                 type="text"
+//                 placeholder="Type here"
+//                 className="input input-bordered w-full h-[44px] focus:outline-none focus:border-customBlue focus:ring-customBlue"
+//               />
+//             </div>
+//             <div className="form-control">
+//               <label className="label text-customBlue">Top CV's</label>
+//               <select className="select select-bordered w-full h-[44px] focus:outline-none focus:border-customBlue focus:ring-customBlue">
+//                 {dropdownsData.topCvs.map((cv, index) => (
+//                   <option key={index} value={cv}>
+//                     {cv}
+//                   </option>
+//                 ))}
+//               </select>
+//             </div>
+//           </div>
+//           <div className="form-control mt-4 md:mt-0 md:flex-1 md:w-[884px]">
+//             <label className="label text-customBlue">Job Description*</label>
+//             <textarea
+//               placeholder="Type here"
+//               className="textarea textarea-bordered w-full h-[147px] resize-none focus:outline-none focus:border-customBlue focus:ring-customBlue"
+//             ></textarea>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Field;
+
 import React from "react";
 import dropdownsData from "../data/dropdownsData.json";
+import imagePaths from "../data/imagePaths.json";
 
 const Field = () => {
   return (
@@ -15,15 +59,25 @@ const Field = () => {
                 className="input input-bordered w-full h-[44px] focus:outline-none focus:border-customBlue focus:ring-customBlue"
               />
             </div>
-            <div className="form-control">
+            <div className="form-control relative">
               <label className="label text-customBlue">Top CV's</label>
-              <select className="select select-bordered w-full h-[44px] focus:outline-none focus:border-customBlue focus:ring-customBlue">
-                {dropdownsData.topCvs.map((cv, index) => (
-                  <option key={index} value={cv}>
-                    {cv}
-                  </option>
-                ))}
-              </select>
+              <div className="relative">
+                <select
+                  className="select select-bordered w-full h-[44px] focus:outline-none focus:border-customBlue focus:ring-customBlue appearance-none pr-10"
+                  style={{ backgroundImage: "none" }}
+                >
+                  {dropdownsData.topCvs.map((cv, index) => (
+                    <option key={index} value={cv}>
+                      {cv}
+                    </option>
+                  ))}
+                </select>
+                <img
+                  src={imagePaths.dropdownArrow}
+                  alt="Dropdown Arrow"
+                  className="absolute right-2 top-1/2  transform -translate-y-1/2 w-[12px] h-[6px] pointer-events-none"
+                />
+              </div>
             </div>
           </div>
           <div className="form-control mt-4 md:mt-0 md:flex-1 md:w-[884px]">
--- src/pages/FormPage.jsx
+++ src/pages/FormPage.jsx
@@ -1,3 +1,37 @@
+// import React, { useState } from "react";
+// import Navbar from "../components/Navbar";
+// import Field from "../components/Field";
+// import CheckingResumeIndex from "../components/CheckingResumeIndex";
+// import UploadResume from "../components/UploadResume";
+// import Footer from "../components/Footer";
+// import RankedResumes from "./RankedResumesPage";
+
+// const FormPage = () => {
+//   const [resumeData, setResumeData] = useState(null);
+
+//   const handleUploadComplete = (data) => {
+//     setResumeData(data);
+//   };
+
+//   if (resumeData) {
+//     return <RankedResumes resumeData={resumeData} />;
+//   }
+
+//   return (
+//     <div className="min-h-screen flex flex-col">
+//       <Navbar />
+//       <div className="flex-grow">
+//         <Field />
+//         <CheckingResumeIndex />
+//         <UploadResume onUploadComplete={handleUploadComplete} />
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default FormPage;
+
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import Field from "../components/Field";
