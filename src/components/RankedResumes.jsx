c4f5089f2164a556135d289bfbbe400baf50f72d
Muhammad Abdullah <abdullahfalak007@gmail.com>
2024-07-04 18:50:04 +0500

    Everything done except for the navbar and footer

--- src/components/RankedResumes.jsx
+++ src/components/RankedResumes.jsx
@@ -1,12 +1,125 @@
+// import React from "react";
+// import PropTypes from "prop-types";
+// import imagePaths from "../data/imagePaths.json"; // Import the image paths
+// import ChatBubble from "../components/ChatBubble"; // Import the ChatBubble component
+
+// const RankedResumes = ({ resumeData }) => {
+//   return (
+//     <div className="flex-grow p-8 relative">
+//       <h2 className="text-3xl font-bold mb-6 text-center">Ranked Resumes</h2>
+//       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+//         {resumeData.map((resume, index) => (
+//           <div
+//             key={index}
+//             className="p-6 border rounded-[17.84px] shadow-lg bg-[#E4F0FF] relative w-[368.22px] h-[370.13px]"
+//           >
+//             <div className="absolute top-4 right-0 flex flex-col space-y-2">
+//               <div className="flex items-center justify-center w-[81.52px] h-[28.82px] rounded-tl-[8.23px] rounded-bl-[8.23px] bg-gradient-to-r from-[#0362B3] to-[#4E9BCF]">
+//                 <img
+//                   src={imagePaths.askAI} // Ensure you have the correct path for the AI icon
+//                   alt="AI Icon"
+//                   className="w-[11.53px] h-[11.53px] mr-1"
+//                 />
+//                 <span className="text-white font-airbnbCereal_W_Bd text-[12.35px]">
+//                   Ask AI
+//                 </span>
+//               </div>
+//               <button className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-3 rounded-tl-[8.23px] rounded-bl-[8.23px] w-[81.52px] h-[28.82px] font-airbnbCereal_W_Bd text-[12.35px]">
+//                 {resume.rankingIndex}% Rank
+//               </button>
+//             </div>
+//             <div className="flex items-center mb-4">
+//               <img
+//                 src={resume.image}
+//                 alt={`${resume.name}'s profile`}
+//                 className="object-cover rounded-full mr-4 mt-2 border-4 border-[#0362B3] w-[45.4px] h-[45.4px]"
+//               />
+//               <div>
+//                 <h3 className="font-poppinsMedium font-normal text-[19.46px]">
+//                   {resume.name}
+//                 </h3>
+//                 <p className="text-gray-600 flex items-center font-interMedium font-extralight text-[12.97px]">
+//                   <img
+//                     src={imagePaths.location}
+//                     alt="Location icon"
+//                     className="h-5 w-5 mr-1"
+//                   />
+//                   {resume.location}
+//                 </p>
+//               </div>
+//             </div>
+//             <div className="mb-4">
+//               <h4 className="font-poppinsMedium font-extralight text-[12.97px] mt-8">
+//                 Skills
+//               </h4>
+//               <div className="text-customBlue font-interMedium font-normal text-[16px]">
+//                 {resume.skills.split(",").map((skill, i) => (
+//                   <span key={i} className="mr-3">
+//                     {skill.trim()}
+//                   </span>
+//                 ))}
+//               </div>
+//             </div>
+//             <div className="mb-4">
+//               <h4 className="font-poppinsMedium font-extralight text-[12.97px] mt-8">
+//                 Total Experience
+//               </h4>
+//               <p className="text-customBlue font-interMedium font-normal text-[16px]">
+//                 {resume.totalExperience}
+//               </p>
+//             </div>
+//             <div className="mb-4">
+//               <h4 className="font-poppinsMedium font-medium text-[12.97px] mt-8">
+//                 Education
+//               </h4>
+//               <p className="text-customBlue font-interMedium font-medium text-[16px]">
+//                 {resume.education}
+//               </p>
+//             </div>
+//             <div className="flex justify-center space-x-4 mt-4">
+//               <button className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-4 rounded-[8.23px] w-[91.82px] h-[28.82px] font-poppinsMedium text-[12.35px] border border-[#0362B3] flex items-center justify-center">
+//                 View
+//               </button>
+//               <button className="bg-gradient-to-r from-[#0362B3] to-[#4E9BCF] text-white py-1 px-4 rounded-[8.23px] w-[91.82px] h-[28.82px] font-poppinsMedium text-[12.35px] border border-[#0362B3] flex items-center justify-center">
+//                 Download
+//               </button>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//       <ChatBubble />
+//     </div>
+//   );
+// };
+
+// RankedResumes.propTypes = {
+//   resumeData: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       name: PropTypes.string.isRequired,
+//       location: PropTypes.string.isRequired,
+//       rankingIndex: PropTypes.number.isRequired,
+//       skills: PropTypes.string.isRequired,
+//       totalExperience: PropTypes.string.isRequired,
+//       education: PropTypes.string.isRequired,
+//       image: PropTypes.string.isRequired, // Added image prop type
+//     })
+//   ).isRequired,
+// };
+
+// export default RankedResumes;
+
 import React from "react";
 import PropTypes from "prop-types";
 import imagePaths from "../data/imagePaths.json"; // Import the image paths
 import ChatBubble from "../components/ChatBubble"; // Import the ChatBubble component
 
-const RankedResumes = ({ resumeData }) => {
+const RankedResumes = ({ resumeData, topCvs }) => {
   return (
     <div className="flex-grow p-8 relative">
-      <h2 className="text-3xl font-bold mb-6 text-center">Ranked Resumes</h2>
+      <h2 className="text-3xl font-poppinsBold text-[40px] text-customBlack mb-12 text-center">
+        <span>Top {topCvs} Ranked</span>
+        <span className="text-customBlue"> Resumes</span>
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {resumeData.map((resume, index) => (
           <div
@@ -104,6 +217,7 @@ RankedResumes.propTypes = {
       image: PropTypes.string.isRequired, // Added image prop type
     })
   ).isRequired,
+  topCvs: PropTypes.string.isRequired,
 };
 
 export default RankedResumes;
--- src/pages/RankedResumesPage.jsx
+++ src/pages/RankedResumesPage.jsx
@@ -1,3 +1,27 @@
+// import React from "react";
+// import Navbar from "../components/Navbar";
+// import Footer from "../components/Footer";
+// import RankedResumes from "../components/RankedResumes";
+// import Field from "../components/Field";
+
+// const RankedResumesPage = ({ resumeData, formData }) => {
+//   return (
+//     <div className="min-h-screen flex flex-col">
+//       <Navbar />
+//       <Field
+//         jobTitle={formData.jobTitle}
+//         topCvs={formData.topCvs}
+//         jobDescription={formData.jobDescription}
+//         onChange={() => {}} // Pass a dummy function as onChange is not needed here
+//       />
+//       <RankedResumes resumeData={resumeData} />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default RankedResumesPage;
+
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -14,7 +38,7 @@ const RankedResumesPage = ({ resumeData, formData }) => {
         jobDescription={formData.jobDescription}
         onChange={() => {}} // Pass a dummy function as onChange is not needed here
       />
-      <RankedResumes resumeData={resumeData} />
+      <RankedResumes resumeData={resumeData} topCvs={formData.topCvs} />
       <Footer />
     </div>
   );
